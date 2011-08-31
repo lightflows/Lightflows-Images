c560e2890671692a365b169b6dcd15a470037f59
Matt Hicks <admin@lightflows.co.uk>
2011-08-31 19:02:12 +0100

    moved everything into root directory

--- lightflows_images/js/lightflows_images.js
+++
@@ -1,8 +0,0 @@
-/*$Id: This code adds the images float property back to the image as a class so it can be themed $*/
-$(document).ready(function(){
-  $(".content img").each(function(i) {
-  	var alignment = $(this).css('float');
-  	alignment = "img"+alignment;
-    $(this).addClass(alignment); /* adds either .imgright or .imgleft class to images */
-  });
-});
